ab6bbeaa54005fff54249faa795e9f5d623312ff
ledouxvi <vincent.l@wearephenix.com>
2020-09-22 13:34:18 +0200

    Update ViewMask.js

--- src/components/ViewMask.js
+++ src/components/ViewMask.js
@@ -47,11 +47,13 @@ class ViewMask extends Component<Props, State> {
                     toValue: size,
                     duration: this.props.animationDuration,
                     easing: this.props.easing,
+                    useNativeDriver: false,
                 }),
                 Animated.timing(this.state.position, {
                     toValue: position,
                     duration: this.props.animationDuration,
                     easing: this.props.easing,
+                    useNativeDriver: false,
                 }),
             ]).start();
         } else {
